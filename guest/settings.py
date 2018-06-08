cf88c805cee43762b4e37262bd22008422f42000
huyn <coool0230@163.com>
2018-06-08 15:25:32 +0800

    1.加入接口

--- guest/settings.py
+++ guest/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8mce9e!5fjc==%h+f6d#k6*yk#l(hr1imy4!k_#dwsq)zy40mf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
